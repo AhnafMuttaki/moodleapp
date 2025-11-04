ab5bb28afd4ddfdd87ad9d4b2d39d322668b5f64
ibnesina1907 <ibne.sina@brainstation-23.com>
2025-11-04 14:23:48 +0600

    fix: appicon and splash icon have been changed

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'moodlemobile',
+  appName: 'eLearning 23',
   webDir: 'www',
   cordova: {
     preferences: {
